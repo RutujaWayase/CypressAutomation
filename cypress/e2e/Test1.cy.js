c6953317d7ac1e480cbc9c485cf1f0533bb0c796
RutujaWayase <rutujawayase1234@gmail.com>
2024-09-05 23:42:19 +0530

    adding test1

--- cypress/e2e/Test1.cy.js
+++ cypress/e2e/Test1.cy.js
@@ -12,7 +12,7 @@ describe('My first test suite', function () {
         cy.get('.product:visible').should('have.length',4) //should is an assertion from Chai library
         //parent child chaining
         cy.get('.products').find('.product').should('have.length', 4)
-
+        cy.get(':nth-child(3) > .product-action > button').click()
         cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
 
 
