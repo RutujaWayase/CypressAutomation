26853efb020fc00a8d2dbcb5c6262e31ba516bcc
RutujaWayase <rutujawayase1234@gmail.com>
2024-09-08 00:59:19 +0530

    adding test1

--- cypress/e2e/Test1.cy.js
+++ cypress/e2e/Test1.cy.js
@@ -14,6 +14,15 @@ describe('My first test suite', function () {
         cy.get('.products').find('.product').should('have.length', 4)
         cy.get(':nth-child(3) > .product-action > button').click()
         cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+
+            const textVeg = $el.find('h4.pproduct-name').text()
+            if(textVeg.includes('Cashews')){
+                $el.find('button').click()
+                //$el.find().click()
+            }
+
+        })
 
 
     })
