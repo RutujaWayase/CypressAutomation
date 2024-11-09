58b5086ea7efdf19a4ebd3c48a931fad03880c40
RutujaWayase <rutujawayase1234@gmail.com>
2024-11-09 23:39:01 +0530

    adding login

---
+++ cypress/e2e/Tests/Login.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress"/>
+
+describe('Validate login functionality', () => {
+    it('Validate login with valid credentials', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get("input[name='username']").type('Admin')
+        cy.get("input[name='password']").type('admin123')
+        cy.get("button[type='submit']").click()
+
+    })
+})
