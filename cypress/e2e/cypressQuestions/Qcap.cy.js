312019f522b37094951a31333f8ed88664fe7cf8
RutujaWayase <rutujawayase1234@gmail.com>
2025-01-07 23:52:33 +0530

    adding ques

---
+++ cypress/e2e/cypressQuestions/Qcap.cy.js
@@ -0,0 +1,21 @@
+
+/*
+fun t (selector){
+    cy.get('nav-search-bar-form'). each($) ( => {
+    cy.wrap($).click();
+    })
+    
+    }
+    
+    cy.get('nav-search-bar-form').click(force: true);
+    
+    cy.get('nav-search-bar-form').invoke('removeAttr', 'target').click();
+    
+    cy.get('nav-search-bar-form').trigger('mousedown', {button:0}).trigger('mousemove')
+    
+    cy.get('')should('be.visible', )
+    cy.wait(100)
+    
+    cy.get('').expect('')
+    
+*/ 
